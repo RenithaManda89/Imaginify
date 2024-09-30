5e22180147db3e8fcef861443e1de803d89a0682
Renitha Manda <mandarenitha@gmail.com>
2024-09-30 08:10:11 +0530

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { UserButton } from '@clerk/nextjs'
+
+const Home = () => {
+  return (
+    <div>
+      <UserButton afterSignOutUrl='/' ></UserButton>
+    </div>
+  )
+}
+
+export default Home
+
+
+
---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Transformations = () => {
+  return (
+    <div> transformations</div>
+  )
+}
+
+export default Transformations
